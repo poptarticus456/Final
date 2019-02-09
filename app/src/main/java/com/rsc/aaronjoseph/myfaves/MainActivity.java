5732ad4da1b95d4e8abc198738332b2025df163c
aaron <Aaron.joseph@outlook.com>
2019-02-09 14:53:11 -0700

    Edits

--- app/src/main/java/com/rsc/aaronjoseph/myfaves/MainActivity.java
+++ app/src/main/java/com/rsc/aaronjoseph/myfaves/MainActivity.java
@@ -3,6 +3,8 @@ package com.rsc.aaronjoseph.myfaves;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ListActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button editMyFaves = (Button)findViewById(R.id.editMyFaves);
+
+
+        editMyFaves.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }

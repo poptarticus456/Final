6affb0eca1c14db31d7de7b34b6994e1508e9bd0
aaron <Aaron.joseph@outlook.com>
2019-02-09 19:10:49 -0700

    Changes to actitivty and UI

--- app/src/main/java/com/rsc/aaronjoseph/myfaves/MainActivity.java
+++ app/src/main/java/com/rsc/aaronjoseph/myfaves/MainActivity.java
@@ -1,12 +1,20 @@
 package com.rsc.aaronjoseph.myfaves;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +22,25 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
         Button editMyFaves = (Button)findViewById(R.id.editMyFaves);
 
+        final ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), listView.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         editMyFaves.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(MainActivity.this, manageMyFaves.class);
+
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/rsc/aaronjoseph/myfaves/manageMyFaves.java
@@ -0,0 +1,19 @@
+package com.rsc.aaronjoseph.myfaves;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class manageMyFaves extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.managemyfaves);
+
+
+    }
+}
